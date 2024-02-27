d6f29bad8bac74b39b2e98a41c6bb4a72c57f4bd
Earl006 <earljoe06@gmail.com>
2024-02-28 01:26:45 +0300

    zee's eccomerce

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,95 @@
+<footer>
+    <!-- Footer Start-->
+    <div class="footer-area footer-padding">
+        <div class="container-fluid ">
+            <div class="row d-flex justify-content-between">
+                <div class="col-xl-3 col-lg-3 col-md-8 col-sm-8">
+                 <div class="single-footer-caption mb-50">
+                   <div class="single-footer-caption mb-30">
+                      <!-- logo -->
+                      <div class="footer-logo mb-35">
+                       <a href="home/index.html"><img src="home/assets/img/logo/logo2_footer.png" alt=""></a>
+                   </div>
+                   <div class="footer-tittle">
+                       <div class="footer-pera">
+                           <p>Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+                       </div>
+                   </div>
+                   <!-- social -->
+                   <div class="footer-social">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="https://bit.ly/sai4ull"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4">
+        <div class="single-footer-caption mb-50">
+            <div class="footer-tittle">
+                <h4>Quick links</h4>
+                <ul>
+                    <li><a href="#">Image Licensin</a></li>
+                    <li><a href="#">Style Guide</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4">
+        <div class="single-footer-caption mb-50">
+            <div class="footer-tittle">
+                <h4>Shop Category</h4>
+                <ul>
+                    <li><a href="#">Image Licensin</a></li>
+                    <li><a href="#">Style Guide</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4">
+        <div class="single-footer-caption mb-50">
+            <div class="footer-tittle">
+                <h4>Pertners</h4>
+                <ul>
+                    <li><a href="#">Image Licensin</a></li>
+                    <li><a href="#">Style Guide</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4">
+        <div class="single-footer-caption mb-50">
+            <div class="footer-tittle">
+                <h4>Get in touch</h4>
+                <ul>
+                    <li><a href="#">(89) 982-278 356</a></li>
+                    <li><a href="#">demo@colorlib.com</a></li>
+                    <li><a href="#">67/A, Colorlib, Green road, NYC</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<!-- footer-bottom area -->
+<div class="footer-bottom-area">
+    <div class="container">
+        <div class="footer-border">
+           <div class="row d-flex align-items-center">
+               <div class="col-xl-12 ">
+                   <div class="footer-copy-right text-center">
+                       <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                          Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- Footer End-->
+</footer>
---
+++ resources/views/home/popular.blade.php
@@ -0,0 +1,58 @@
+<div class="popular-items pt-50">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-popular-items mb-50 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
+                            <div class="popular-img">
+                                <img src="home/assets/img/gallery/popular1.png" alt="">
+                                <div class="img-cap">
+                                    <span>Glasses</span>
+                                </div>
+                                <div class="favorit-items">
+                                 <a href="shop.html" class="btn">Shop Now</a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-popular-items mb-50 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
+                        <div class="popular-img">
+                            <img src="home/assets/img/gallery/popular2.png" alt="">
+                            <div class="img-cap">
+                                <span>Watches</span>
+                            </div>
+                            <div class="favorit-items">
+                             <a href="home/shop.html" class="btn">Shop Now</a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="single-popular-items mb-50 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">
+                    <div class="popular-img">
+                        <img src="home/assets/img/gallery/popular3.png" alt="">
+                        <div class="img-cap">
+                            <span>Jackets</span>
+                        </div>
+                        <div class="favorit-items">
+                         <a href="home/shop.html" class="btn">Shop Now</a>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="single-popular-items mb-50 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">
+                <div class="popular-img">
+                    <img src="home/assets/img/gallery/popular4.png" alt="">
+                    <div class="img-cap">
+                        <span>Clothes</span>
+                    </div>
+                    <div class="favorit-items">
+                     <a href="home/shop.html" class="btn">Shop Now</a>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+</div>
+</div>
